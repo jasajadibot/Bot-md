9a2667e4bfcfdac4427fea7e7ed6eb899b2966bb
jasajadibot <113238988+jasajadibot@users.noreply.github.com>
2022-10-14 18:10:00 +0700

    Update indonesia.js

--- language/indonesia.js
+++ language/indonesia.js
@@ -841,8 +841,8 @@ Goodbye`
 exports.source = () =>{
 return`*------「 SOURCE CODE 」 ------*
 
-Base : https://github.com/DikaArdnt/Hisoka-Morou
-Recode : https://youtube.com/playlist?list=PLFCglBzFPHG7vSQaa9S3P8yfla_d9w-2-
+Base : https://yandex.com
+Recode : https://yandex.com
 `
 }
 exports.tos = (ownernomer) => {
